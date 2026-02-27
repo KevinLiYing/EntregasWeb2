750d318c6fe28abcd103291e16c371a50105d707
KevinLi <unusualCosmonaut@gmail.com>
2026-02-27 11:30:46 +0100

    Movie model añadido (esquema)

---
+++ EjercicioT5/src/models/movie.model.js
@@ -0,0 +1,64 @@
+import mongoose from 'mongoose';
+/*{
+  title: String,        // Requerido, mín 2 caracteres
+  director: String,     // Requerido
+  year: Number,         // Entre 1888 y año actual
+  genre: String,        // Enum: action, comedy, drama, horror, scifi
+  copies: Number,       // Total de copias (default: 5)
+  availableCopies: Number, // Copias disponibles
+  timesRented: Number,  // Contador de alquileres (default: 0)
+  cover: String         // Nombre del archivo de carátula (default: null)
+}*/
+const movieSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, 'El nombre es requerido'],
+      minlength: [2, 'Mínimo 2 caracteres'],
+    },
+    director: {
+      type: String,
+      require: [true, "El director es requerido"]
+    },
+    year: {
+      type: Number,
+      min: [1888, "Año introducido no válido"]
+    },
+    genre: {
+      type: String,
+      enum: ["action", "comedy" , "drama", "horror", "scifi"],
+    required: [true, "Introduce un género"]
+    },
+    copies: {
+      type: Number,
+      default: 5,
+      min: [0, "No puede ser negativo"]
+    },
+    availableCopies: {
+      type: Number,
+      default: 5,
+      min: [0, "No puede ser negativo"]
+    },
+    timesRented: {
+      type: Number,
+      default: 0,
+      min: 0
+    },
+    cover: {
+      type: String,
+      default: null
+    }
+
+  },
+  {
+    timestamps: true,   // Añade createdAt y updatedAt
+    versionKey: false   // Elimina __v
+  }
+);
+
+// Probando a usar indices
+movieSchema.index({ title: 'text' });
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+export default Movie;
