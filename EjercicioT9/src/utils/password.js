ffc3030d510f012f59c9e3fcf3c4f6363b8397bf
KevinLi <unusualCosmonaut@gmail.com>
2026-04-10 13:47:16 +0200

    Password.js

--- EjercicioT9/src/utils/password.js
+++ EjercicioT9/src/utils/password.js
@@ -1,22 +1,9 @@
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcrypt';
 
-const SALT_ROUNDS = 10;
-
-/**
- * Hashea una contraseña en texto plano
- * @param {string} plainPassword
- * @returns {Promise<string>} hash
- */
-export async function hashPassword(plainPassword) {
-	return await bcrypt.hash(plainPassword, SALT_ROUNDS);
+export async function hashPassword(password) {
+	return bcrypt.hash(password, 10);
 }
 
-/**
- * Compara una contraseña en texto plano con su hash
- * @param {string} plainPassword
- * @param {string} hash
- * @returns {Promise<boolean>}
- */
-export async function verifyPassword(plainPassword, hash) {
-	return await bcrypt.compare(plainPassword, hash);
+export async function comparePassword(password, hash) {
+	return bcrypt.compare(password, hash);
 }
