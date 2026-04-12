cc6d670eef4500ac644ffb941ec6ab29d04c06a3
KevinLi <unusualCosmonaut@gmail.com>
2026-04-12 20:28:46 +0200

    notification service

--- PracticaIntermedia/src/services/notification.service.js
+++ PracticaIntermedia/src/services/notification.service.js
@@ -6,16 +6,16 @@ const notificationService = new NotificationService();
 
 // Listeners for user lifecycle events
 notificationService.on('user:registered', (user) => {
-  console.log(`📢 Evento: user:registered para ${user.email}`);
+  console.log(`Evento: user:registered para ${user.email}`);
 });
 notificationService.on('user:verified', (user) => {
-  console.log(`📢 Evento: user:verified para ${user.email}`);
+  console.log(`Evento: user:verified para ${user.email}`);
 });
 notificationService.on('user:invited', (user) => {
-  console.log(`📢 Evento: user:invited para ${user.email}`);
+  console.log(`Evento: user:invited para ${user.email}`);
 });
 notificationService.on('user:deleted', (user) => {
-  console.log(`📢 Evento: user:deleted para ${user.email}`);
+  console.log(`Evento: user:deleted para ${user.email}`);
 });
 
 export default notificationService;
